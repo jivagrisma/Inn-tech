5ec233b998273587abb8c9e2e22184fc0e1e93bd
jivagrisma <jivagrisma@gmail.com>
2024-12-09 21:27:19 -0500

    se guardan cambios realizados

--- assets/utils/content.js
+++ assets/utils/content.js
@@ -20,6 +20,7 @@ const clients = [
 const explore = [
   {
     url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg",
+<<<<<<< HEAD
     alt: "Graphics & Design"
   },
   {
@@ -53,17 +54,57 @@ const explore = [
   {
     url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg",
     alt: "Data"
+=======
+    alt: "Diseño Gráfico"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg",
+    alt: "Marketing Digital"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg",
+    alt: "Redacción y traducción"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg",
+    alt: "Video & Animación"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg",
+    alt: "Música & Audio"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg",
+    alt: "Programación y Tecnología"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg",
+    alt: "Negocios"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg",
+    alt: "Estilo de vida"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg",
+    alt: "Datos"
+>>>>>>> 0554093 (se guardan cambios realizados)
   },
 ];
 
 const services = [
   {
     url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741668/seo-2x.png",
+<<<<<<< HEAD
     moto: "Unlock growth online",
+=======
+    moto: "Desbloquee el crecimiento en línea",
+>>>>>>> 0554093 (se guardan cambios realizados)
     head: "SEO"
   },
   {
     url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/illustration-2x.png",
+<<<<<<< HEAD
     moto: "Color your dreams",
     head: "Illustration"
   },
@@ -90,10 +131,39 @@ const services = [
   {
     url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png",
     moto: "Customize your site",
+=======
+    moto: "Colorea tus sueños",
+    head: "Ilustración"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741674/translation-2x.png",
+    moto: "Global",
+    head: "Traducción"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741664/data-entry-2x.png",
+    moto: "Conozca su negocio",
+    head: "Análisis de datos"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/book-covers-2x.png",
+    moto: "Muestra tu historia",
+    head: "Portadas de libros"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/logo-design-2x.png",
+    moto: "Construye tu marca",
+    head: "Diseño de logotipo"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/ae11e2d45410b0eded7fba0e46b09dbd-1598561917003/wordpress-2x.png",
+    moto: "Personaliza tu sitio",
+>>>>>>> 0554093 (se guardan cambios realizados)
     head: "Wordpress"
   },
   {
     url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741669/voiceover-2x.png",
+<<<<<<< HEAD
     moto: "Share your message",
     head: "Voice Over"
   },
@@ -105,6 +175,19 @@ const services = [
   {
     url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png",
     moto: "Reach more customers",
+=======
+    moto: "Comparte tu mensaje",
+    head: "Voz en off"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741663/animated-explainer-2x.png",
+    moto: "Involucre a su audiencia",
+    head: "Vídeo explicativo"
+  },
+  {
+    url: "https://fiverr-res.cloudinary.com/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741667/social-2x.png",
+    moto: "Llegar a más clientes",
+>>>>>>> 0554093 (se guardan cambios realizados)
     head: "Social Media"
   },
 ]
